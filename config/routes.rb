e6b8d2a733ab0d3c82afbe257a684781023bacc9
Aishwarya Pakeyara <apakeyara@gmail.com>
2022-06-29 00:11:33 +0530

    Config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'invitations/index'
-  get 'invitations/new'
-  get 'invitations/create'
+  get "user/index"
+  get "user/new"
+  get "user/create"
+  get "invitations/index"
+  get "invitations/new"
+  get "invitations/create"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
