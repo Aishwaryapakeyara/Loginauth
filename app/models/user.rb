6d0089d172920fc37b397db6164da79711c5a5e2
Aishwarya Pakeyara <apakeyara@gmail.com>
2022-06-29 21:33:22 +0530

    App files

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable, :confirmable
+  belongs_to :role
 end
